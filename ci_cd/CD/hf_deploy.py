e079d5a4696027d6ef871dcf1c5c3a4f9499d140
francescofrigerio <54633008+francescofrigerio@users.noreply.github.com>
2026-06-15 13:56:17 +0000

    fix model repo_id on hf_dceploy

--- ci_cd/CD/hf_deploy.py
+++ ci_cd/CD/hf_deploy.py
@@ -5,7 +5,8 @@ from huggingface_hub import upload_folder
 model_folder = os.environ.get("MODEL_OUT_DIR", "src/outputs-baseline-prod")
 
 upload_folder(  folder_path=model_folder,
-                repo_id="MachineInnovation/twitter-sentiment-model",
+                # repo_id="MachineInnovation/twitter-sentiment-model",
+                repo_id="francescofrigerio/machine-innovation-sentiment-model",
                 repo_type="model",
                 token=os.environ["HF_TOKEN"]
              )
