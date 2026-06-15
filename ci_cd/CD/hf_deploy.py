0172b96ab32c1673cb3123ec7ff316ab7fd54699
francescofrigerio <54633008+francescofrigerio@users.noreply.github.com>
2026-06-15 13:19:03 +0000

    fix output folder on hf deploy

--- ci_cd/CD/hf_deploy.py
+++ ci_cd/CD/hf_deploy.py
@@ -1,7 +1,10 @@
 import os
 from huggingface_hub import upload_folder
 
-upload_folder(  folder_path="output",
+# Prende la cartella definita nel workflow, altrimenti usa prod di default
+model_folder = os.environ.get("MODEL_OUT_DIR", "src/outputs-baseline-prod")
+
+upload_folder(  folder_path=model_folder,
                 repo_id="MachineInnovation/twitter-sentiment-model",
                 repo_type="model",
                 token=os.environ["HF_TOKEN"]
